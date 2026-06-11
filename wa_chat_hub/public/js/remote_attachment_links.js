49f0e2c5866eeabebe02ab60f211094c474ad55b
SRDevPortal <webdevelopersriaas@gmail.com>
2026-06-11 16:12:18 +0530

    Update remote attachment links script

--- wa_chat_hub/public/js/remote_attachment_links.js
+++ wa_chat_hub/public/js/remote_attachment_links.js
@@ -1,6 +1,38 @@
 (function () {
 	let attempts = 0;
 
+	function isRemoteUrl(value) {
+		return /^https?:\/\//i.test(value || "");
+	}
+
+	function normalizeRemoteUrl(value) {
+		if (!isRemoteUrl(value)) {
+			return value;
+		}
+		let url = value.replace(/#/g, "%23");
+		for (let i = 0; i < 2 && /%25[0-9a-f]{2}/i.test(url); i += 1) {
+			try {
+				url = decodeURI(url);
+			} catch (error) {
+				break;
+			}
+		}
+		return url;
+	}
+
+	function patchRenderedLinks(root) {
+		if (!root || !root.querySelectorAll) {
+			return;
+		}
+		root.querySelectorAll('a[href^="http://"], a[href^="https://"]').forEach((link) => {
+			const rawHref = link.getAttribute("href") || "";
+			const normalized = normalizeRemoteUrl(rawHref);
+			if (normalized && normalized !== rawHref) {
+				link.setAttribute("href", normalized);
+			}
+		});
+	}
+
 	function applyPatch() {
 		attempts += 1;
 		if (!window.frappe || !frappe.ui || !frappe.ui.form || !frappe.ui.form.Attachments) {
@@ -18,12 +50,24 @@
 		const originalGetFileUrl = proto.get_file_url;
 		proto.get_file_url = function (attachment) {
 			const fileUrl = (attachment && attachment.file_url) || "";
-			if (/^https?:\/\//i.test(fileUrl)) {
-				return fileUrl.replace(/#/g, "%23");
+			if (isRemoteUrl(fileUrl)) {
+				return normalizeRemoteUrl(fileUrl);
 			}
 			return originalGetFileUrl.call(this, attachment);
 		};
 		proto.__wa_chat_hub_remote_url_patch = true;
+
+		if (frappe.router && frappe.router.on) {
+			frappe.router.on("change", () => setTimeout(() => patchRenderedLinks(document), 500));
+		}
+		if (window.MutationObserver) {
+			new MutationObserver((mutations) => {
+				mutations.forEach((mutation) => {
+					mutation.addedNodes.forEach((node) => patchRenderedLinks(node));
+				});
+			}).observe(document.body, { childList: true, subtree: true });
+		}
+		patchRenderedLinks(document);
 	}
 
 	applyPatch();
